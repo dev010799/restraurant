a5b3825a7f470bc3962f3dad44e56b5d93e319c3
Dev Mishra <dev010799@gmail.com>
2023-10-11 02:42:09 +1200

    basic restraurant app in react

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Restraurant from "./component/Basics/Restraurant";
+
+const App = () => {
+  return <Restraurant />;
+};
+
+export default App;
+
+//********************************************************************************* */
---
+++ src/component/Basics/Restraurant.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./style.css";
+// import Restraurant from "./component/Basics/Restraurant";
+
+ const Restraurant = () => {
+  return (
+    <>
+        <div className="card-container">
+          <div className="card">
+            <div className="card-body">
+              <span className="card-number card-circle subtle">1</span>
+              <span className="card-author subtle" style= {{color: "red"}}>Breakfast</span>
+            </div>
+          </div>
+
+        </div>
+    </>  
+  );
+};
+
+export default Restraurant;
+
+// *************************************************************************
+
+
